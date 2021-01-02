ccafef52f99b702a682ea236f461a0ba02b450f7
martycook <martinc@valornet.com>
2021-01-02 07:54:25 -0600

    bug fixes

--- src/CG.Sms/ApplicationBuilderExtensions.cs
+++ src/CG.Sms/ApplicationBuilderExtensions.cs
@@ -24,11 +24,14 @@ namespace CG.Sms
         /// the operation.</param>
         /// <param name="hostEnvironment">The host environment to use for the 
         /// operation.</param>
+        /// <param name="configurationSection">The configuration section to use 
+        /// for the operation.</param>
         /// <returns>Ther value of the <paramref name="applicationBuilder"/>
         /// parameter, for chaining calls together.</returns>
         public static IApplicationBuilder UseEmail(
             this IApplicationBuilder applicationBuilder,
-            IWebHostEnvironment hostEnvironment
+            IWebHostEnvironment hostEnvironment,
+            string configurationSection
             )
         {
             // Validate the parameters before attempting to use them.
@@ -36,7 +39,10 @@ namespace CG.Sms
                 .ThrowIfNull(hostEnvironment, nameof(hostEnvironment));
 
             // Call the use method for the strategy.
-            applicationBuilder.UseStrategies(hostEnvironment);
+            applicationBuilder.UseStrategies(
+                hostEnvironment,
+                configurationSection
+                );
 
             // Return the applicationn builder.
             return applicationBuilder;
--- src/CG.Sms/ServiceCollectionExtensions.cs
+++ src/CG.Sms/ServiceCollectionExtensions.cs
@@ -25,7 +25,7 @@ namespace Microsoft.Extensions.DependencyInjection
         /// </summary>
         /// <param name="serviceCollection">The service collection to use for 
         /// the operation.</param>
-        /// <param name="configuration">the configuration to use for the operation.</param>
+        /// <param name="configuration">The configuration to use for the operation.</param>
         /// <returns>A <see cref="IServiceCollection"/> object for building up
         /// an strategies for the service.</returns>
         /// <exception cref="ArgumentException">This exception is thrown whenever
@@ -38,47 +38,6 @@ namespace Microsoft.Extensions.DependencyInjection
             // Validate the parameters before attempting to use them.
             Guard.Instance().ThrowIfNull(serviceCollection, nameof(serviceCollection))
                 .ThrowIfNull(configuration, nameof(configuration));
-
-            // Check the configuration path, just in case we need to adjust it, to 
-            //   work with this method.
-
-            // Get the path for the current section.
-            var path = configuration.GetPath();
-
-            // Were we called with the configuration root?
-            if (true == string.IsNullOrEmpty(path))
-            {
-                // Point to the proper configuration section.
-                configuration = configuration.GetSection("Services:Sms");
-
-                // Get the new path.
-                path = configuration.GetPath();
-            }
-            else if (false == path.EndsWith("Sms"))
-            {
-                // Point to the proper configuration section.
-                configuration = configuration.GetSection("Sms");
-
-                // Get the new path.
-                path = configuration.GetPath();
-            }
-
-            // Now we should be pointed to the SMS section, but, just in case, 
-            //   let's do one more check. We're doing this here because configuration
-            //   bugs are difficult and frustrating to troubleshoot, so, we want to 
-            //   provide as much feedback as is practical to the caller.
-
-            if (false == path.EndsWith("Sms"))
-            {
-                // Panic!
-                throw new ConfigurationException(
-                    message: string.Format(
-                        Resources.NotSmsSection,
-                        nameof(AddSms),
-                        path
-                        )
-                    );
-            }
 
             // Register the service.
             serviceCollection.AddSingleton<ISmsService, SmsService>();
